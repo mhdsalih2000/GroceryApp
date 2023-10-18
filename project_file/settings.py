621adfd11d70ed22cb36aab556df012c4afa3966
muhammed salih <muhammedswalih034@gmail.com>
2023-10-18 15:31:38 +0530

    custom user added

--- project_file/settings.py
+++ project_file/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Account',
+    'app1',
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
